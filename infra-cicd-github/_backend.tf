76121abf0581db73fd390ce78abf4737e844d2f2
Sancho <johndoe@example.com>
2024-10-23 11:31:16 -0300

    Ajuste backend cicd

--- infra-cicd-github/_backend.tf
+++ infra-cicd-github/_backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "${var.organization_name}-terraform-state"
+    bucket = "sancho-terraform-state"
     key    = "state/aws/cicd-github/terraform.tfstate"
     region = "us-east-1"
   }
